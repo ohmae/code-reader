11a2cbfc9802bd374aacd46e5c7fada97e235e86
OHMAE Ryosuke <ryo@mm2d.net>
2021-06-05 11:05:35 +0900

    🆙libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,17 +69,17 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.camera:camera-camera2:1.0.0")
     implementation("androidx.camera:camera-lifecycle:1.0.0")
-    implementation("androidx.camera:camera-view:1.0.0-alpha24")
+    implementation("androidx.camera:camera-view:1.0.0-alpha25")
     implementation("androidx.browser:browser:1.3.0")
     implementation("androidx.webkit:webkit:1.4.0")
-    implementation("com.google.mlkit:barcode-scanning:16.1.1")
+    implementation("com.google.mlkit:barcode-scanning:16.1.2")
     implementation("com.jakewharton.timber:timber:4.7.1")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.91.1")
+    debugImplementation("com.facebook.flipper:flipper:0.92.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.91.1")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.91.1")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.92.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.92.0")
 }
 
 fun isStable(version: String): Boolean {
