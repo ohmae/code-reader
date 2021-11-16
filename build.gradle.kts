174405b3f7522a77aea283b7640e683347a977ed
OHMAE Ryosuke <ryo@mm2d.net>
2021-11-16 23:55:52 +0900

    🆙kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.3")
-        classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath(kotlin("gradle-plugin", version = "1.6.0"))
         classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
     }
 }
