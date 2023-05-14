e00962a9bb5505d8806c632742bdb5569932b1ae
OHMAE Ryosuke <ryo@mm2d.net>
2023-05-14 10:36:33 +0900

    🆙Jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.7.1")
     implementation("androidx.fragment:fragment-ktx:1.5.7")
