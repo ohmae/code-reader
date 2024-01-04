79ea02cab8001f218d9a241b034cb4767ef813f1
OHMAE Ryosuke <ryo@mm2d.net>
2024-01-04 22:50:33 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,9 +88,9 @@ dependencies {
     implementation("com.google.mlkit:barcode-scanning:17.2.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.13")
     debugImplementation("com.facebook.flipper:flipper:0.243.0")
-    debugImplementation("com.facebook.soloader:soloader:0.10.5")
+    debugImplementation("com.facebook.soloader:soloader:0.11.0")
     debugImplementation("com.facebook.flipper:flipper-network-plugin:0.243.0")
     debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.243.0")
 
