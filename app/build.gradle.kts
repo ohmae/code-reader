7c4a3571ed34b2543691a5a2bb2a1af4928f5d08
OHMAE Ryosuke <ryo@mm2d.net>
2024-01-05 11:05:54 +0900

    🎉release 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 
 val applicationName = "CodeReader"
 val versionMajor = 0
-val versionMinor = 1
-val versionPatch = 4
+val versionMinor = 2
+val versionPatch = 0
 
 android {
     compileSdk = 34
