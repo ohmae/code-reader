3b9cd41025cf000b06b2a8ece359ef4b1f625023
OHMAE Ryosuke <ryo@mm2d.net>
2026-07-31 21:38:37 +0900

    🆙targetSdk 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "net.mm2d.codereader"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
         versionName = "$versionMajor.$versionMinor.$versionPatch"
         base.archivesName.set("$applicationName-$versionName")
