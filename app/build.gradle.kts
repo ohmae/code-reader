3f10bde15c3312ea2b1217a5a849b9d928199e77
OHMAE Ryosuke <ryo@mm2d.net>
2026-09-25 18:33:02 +0900

    release version 0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val applicationName = "CodeReader"
 val versionMajor = 0
 val versionMinor = 5
-val versionPatch = 3
+val versionPatch = 4
 
 android {
     namespace = "net.mm2d.codereader"
